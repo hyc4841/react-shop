511f8cd86b01a2ba4d2364d5e87fd42e7134df3f
hyc4841 <dbscjf4841@naver.com>
2024-12-17 00:41:10 +0900

    헤더 로그인 유지 작성중

--- src/component/ui/Header.jsx
+++ src/component/ui/Header.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Container, Col, Row, Button } from "react-bootstrap";
+import axios from "axios";
+
+import useFetch from "../../requestAPI/useFetch";
 
 const Header = () => {
+
+    const [data, setData] = useState([]); // 데이터 상태
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null); // 에러 상태
+
+    useEffect(() => {
+        var fetchdata = useFetch("http://localhost:8080/member/islogin");
+        setData(fetchdata.data);
+        setError(fetchdata.error);
+        setLoading(fetchdata.loading);
+    }, []);
+
+    var isLoggedIn
+
+
     return (
         <header>
-            <a href="/">홈</a>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <a href="/board">게시판</a>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <a href="/login">로그인</a>
-            &nbsp;&nbsp;|&nbsp;&nbsp;
-            <a href="/signup">회원가입</a>
+            <Container>
+                <Row>
+                    <Col>
+                        <a href="/">홈</a>
+                        &nbsp;&nbsp;|&nbsp;&nbsp;
+                        <a href="/board">게시판</a>
+                        &nbsp;&nbsp;|&nbsp;&nbsp;
+                    </Col>
+
+                    <Col>
+                        <a href="/login">로그인</a>
+                        &nbsp;&nbsp;|&nbsp;&nbsp;
+                        <a href="/signup">회원가입</a>
+                    </Col>
+
+
+                </Row>
+            </Container>
             <hr/>
         </header>
     );
