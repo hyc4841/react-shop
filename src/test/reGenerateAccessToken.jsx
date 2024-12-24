533888e44809b0d54d51a466502ffcb012279409
hyc4841 <dbscjf4841@naver.com>
2024-12-24 17:54:26 +0900

    클라이언트에서 헤더값 읽기

--- src/test/reGenerateAccessToken.jsx
+++ src/test/reGenerateAccessToken.jsx
@@ -4,6 +4,8 @@ import axios from "axios";
 
 const ReGenerateAccessToken = () => {
 
+    var accessToken;
+
     const axiosIns = axios.create({
         baseURL: 'http://localhost:8080',
         headers: {
@@ -18,9 +20,17 @@ const ReGenerateAccessToken = () => {
             // 만료된 엑세스 토큰을 서버로 들고 가는것
             console.log("리프레시 토큰으로 엑세스 토큰 재발급 받기 테스트");
             const response = await axiosIns.get('/refresh');
-            console.log(response.data.accessToken);
+
+            console.log(response.headers['access-token']);
+
+            accessToken = response.headers['access-token']
+
+            localStorage.setItem('accessToken', accessToken);
+            
         } catch (err) {
             console.error(err);
+        } finally {
+            console.log(accessToken);
         }
     }
 
