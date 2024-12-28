4f22ddab801036d4904d73cd924d4d11c17c560d
hyc4841 <dbscjf4841@naver.com>
2024-12-29 08:10:20 +0900

    코드 수정

---
+++ src/redux/counter.jsx
@@ -0,0 +1,19 @@
+export const INCRESE = "COUNT/INCRESE";
+
+export const increseCount = count => ({ type: INCRESE, count });
+
+const initalState = {
+    count: 0
+};
+
+const counter = (state = initalState, action) => {
+    switch (action.type) {
+        case INCRESE:
+            return {
+                ...state,
+                count: action.count
+            };
+            default:
+                return state;
+    }
+};
