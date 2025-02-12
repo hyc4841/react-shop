2685132a73832fbdb3329485aac34b191dc570d5
hyc4841 <dbscjf4841@naver.com>
2025-02-12 20:13:58 +0900

    비밀번호 수정 제출 버튼 생성

---
+++ src/component/page/member/PasswordChangButton.jsx
@@ -0,0 +1,41 @@
+import axios from "axios";
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const PasswordChangeButton = (props) => {
+
+    const { curPwd, newPwd, newPwdCon } = props;
+    const navigate = useNavigate();
+    
+    const submitPasswordChange = async () => {
+
+        try {
+            const response = await axios.post('http://localhost:8080/member/password', 
+                {curPwd, newPwd, newPwdCon},
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+                        'Content-Type': 'application/json',
+                    },
+                    withCredentials: true
+                });
+
+            console.log(response);
+            alert("비밀번호가 성공적으로 변경 되었습니다.");
+            navigate('/');
+        } catch (error) {
+
+            console.error("비밀번호 변경 오류 : " + error);
+        }
+
+    }
+
+    return (
+        <Button type="button" onClick={submitPasswordChange}>
+            비밀번호 변경
+        </Button>
+    );
+};
+
+export default PasswordChangeButton;
