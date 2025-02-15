7576a6abd279d8b92e5222db8fe5bea197932f06
hyc4841 <dbscjf4841@naver.com>
2025-02-16 01:04:19 +0900

    이메일 변경 추가

--- src/component/page/member/MemberInfoPage.jsx
+++ src/component/page/member/MemberInfoPage.jsx
@@ -9,6 +9,7 @@ import MypageNav from "../../page/member/MyPageNav";
 import '../../../css/memberInfoPage.css';
 import PasswordChangeButton from "./PasswordChangButton";
 import LoginChangeButton from "./LoginIdChangeButton";
+import EmailChangeButton from "./EmailChangeButton";
 
 const MemberInfo = (props) => {
     const navigate = useNavigate();
@@ -19,14 +20,15 @@ const MemberInfo = (props) => {
     const [ newPwdCon, setNewPwdCon ] = useState("");
 
     const [ newLoginId, setNewLoginId ] = useState("");
+    const [ newEmail , setNewEmail ] = useState("");
 
-    const [ loginIdChange, setLoginIdChange ] = useState(false);
-    const [ loginIdChangeTitle, setLoginIdChangeTitle ] = useState("변경하기")
+    const [ loginIdChange, setLoginIdChange ] = useState(false); // 아이디 변경 버튼 플레그
+    const [ loginIdChangeTitle, setLoginIdChangeTitle ] = useState("변경하기");
     const [ loginChangeError, setLoginChangeError ] = useState(null);
 
-    const [ emailChange, setEmailChange ] = useState(false);
-    const [ emailChangeTitle, setEmailChangeTitle ] = useState("변경하기")
-    const [ emailhangeError, setEmailChangeError ] = useState(null);
+    const [ emailChange, setEmailChange ] = useState(false); // 이메일 변경 버튼 플레그
+    const [ emailChangeTitle, setEmailChangeTitle ] = useState("변경하기"); // 변경 버튼 텍스트
+    const [ emailChangeError, setEmailChangeError ] = useState(null);
 
 
     useEffect(() => {
@@ -97,18 +99,20 @@ const MemberInfo = (props) => {
                         <tr>
                             <td>아이디</td>
                             <td>
-                                <div style={{marginBottom:"15px"}}>
+                                <div>
                                     {memberData ? memberData.loginId : ""} <Button onClick={loginIdChangeBtn}>{loginIdChangeTitle}</Button>
                                 </div>
 
                                 {loginIdChange &&
                                     <>
-                                        <div style={{display: "flex"}}>
-                                            <input className="form-control" onChange={(e) => setNewLoginId(e.target.value)}
+                                        <div style={{display: "flex", marginTop: "15px"}}>
+                                            <input className="form-control" type="text"
+                                            onChange={(e) => setNewLoginId(e.target.value)}
                                             style={{width: "auto", marginRight: "10px"}} />
                                             <LoginChangeButton 
                                                 newLoginId={newLoginId}
                                                 onChangeError={fetchLoginIdError}
+                                                fetchMemberData={setMemberData}
                                             />
                                         </div>
 
@@ -128,12 +132,31 @@ const MemberInfo = (props) => {
                             <td>이메일</td>
                             <td>
                                 <div>
-                                    {memberData && memberData.email} <Button>변경</Button>
+                                    {memberData && memberData.email} <Button type="button" onClick={emailChangeBtn}>{emailChangeTitle}</Button>
                                 </div>
+                                
+                                {emailChange && 
+                                    <>
+                                        <div style={{display: "flex", marginTop: "15px"}}>
 
-                                <div>
-                                    
-                                </div>
+                                            <input className="form-control" type="email" onChange={(e) => setNewEmail(e.target.value)}
+                                            style={{width: "auto", marginRight: "10px"}} />
+
+                                            <EmailChangeButton
+                                                newEmail={newEmail}
+                                                onChangeError={setEmailChangeError}
+                                                fetchMemberData={setMemberData}
+                                            />
+                                        </div>
+
+                                        {emailChangeError && 
+                                            <div style={{marginTop: "15px"}}>
+                                                <p style={{color: "red"}}>{emailChangeError.newEmail}</p>
+                                            </div>
+                                        
+                                        }
+                                    </>
+                                }
                                 
                             
                             </td>
@@ -142,13 +165,13 @@ const MemberInfo = (props) => {
                         {/* 이름 변경 */}        
                         <tr>
                             <td>이름</td>
-                            <td>{memberData && memberData.name}</td>
+                            <td>{memberData && memberData.name} <Button>변경하기</Button></td>
                         </tr>
                         {/* 휴대폰 번호 변경 */}
 
                         <tr>
                             <td>휴대폰 번호</td>
-                            <td>{memberData && memberData.phoneNum}</td>
+                            <td>{memberData && memberData.phoneNum} <Button>변경하기</Button></td>
                         </tr>
 
                         <tr>
@@ -189,6 +212,7 @@ const MemberInfo = (props) => {
                                                     curPwd={curPwd}
                                                     newPwd={newPwd}
                                                     newPwdCon={newPwdCon}
+                                                    fetchMemberData={setMemberData}
                                                 />
                                             </td>
                                         </tr>
