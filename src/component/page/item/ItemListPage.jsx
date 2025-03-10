c44327a339e16c9aae87e76061cba0ef93691204
hyc4841 <dbscjf4841@naver.com>
2025-03-10 23:33:11 +0900

    ItemListPage 추가

---
+++ src/component/page/item/ItemListPage.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Container, Nav } from "react-bootstrap";
+
+const ItemListPage = () => {
+
+
+    return (
+        <Container className="d-flex">
+            <div className="d-flex flex-column flex-shrink" style={{width: "280px"}}>
+                <a href="#" className="d-flex align-items-center text-decoration-none">
+                    <span>사이드 바</span>
+
+                </a>
+
+                <hr />
+
+                
+                
+            </div>
+
+        </Container>
+    );
+    
+
+};
+
+export default ItemListPage;
