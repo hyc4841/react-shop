a7d5feb7df513d9cf2f0d85bdc18f1211c9b0c8c
hyc4841 <dbscjf4841@naver.com>
2025-03-13 23:37:59 +0900

    아이템 리스트 페이지 데이터 요청 추가

--- src/component/page/item/ItemListPage.jsx
+++ src/component/page/item/ItemListPage.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { Container, Nav } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
 
 const ItemListPage = () => {
+
+    const location = useLocation();
+    const query = new URLSearchParams(location.search);
+    const categoryId = query.get('category');
+
+    const categories = [categoryId];
+    const itemName = null;
+    const morePrice = null;
+    const lessPrice = null;
+
+    const searchCond = {categories, itemName, morePrice, lessPrice};
+
+    console.log(searchCond);
+
+    // 나중에는 검색 태그 같은것들을 바디에 넣어서 조회
+
+    useEffect(() => {
+        const fetchItems = async () => {
+            try {
+                const response = await axios.get(`http://localhost:8080/items`, {
+                    params: {
+                        categories: categoryId,
+                        itemName: itemName,
+                        morePrice: morePrice,
+                        lessPrice: lessPrice
+                }},  
+                {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                    },
+                    withCredentials: true
+                });
+
+                console.log("dfdfd");
+                console.log(response.data);
+                                                
+            } catch (error) {
+                console.log(error);
+            }
+        };
+
+        fetchItems();
+    });
 
 
     return (
