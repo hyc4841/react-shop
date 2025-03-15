d2b5f2141e0bbe942358e9e3e9dbb1f66f78af98
hyc4841 <dbscjf4841@naver.com>
2025-03-15 23:08:15 +0900

    카테고리 네비게이션 컴포넌트화

--- src/component/page/MainPage.jsx
+++ src/component/page/MainPage.jsx
@@ -9,6 +9,9 @@ import axios from "axios";
 import Category2Depth from "./item/Category2Depth";
 
 import "../../css/categoryNav.css";
+import "../../css/mainPage.css"
+import { Container } from "react-bootstrap";
+import CategoryNav from "./item/CategoryNav";
 
 const Wrapper = styled.div`
     padding: 16px;
@@ -19,104 +22,18 @@ const Wrapper = styled.div`
     justify-content: center;
 `;
 
-const Container = styled.div`
-    width: 100%;
-    max-width: 720px;
-
-    & > * {
-        :not(:last-child) {
-            margin-bottom: 16px;
-        }
-    }
-`;
-
 
 function MainPage(props) {
     const {} = props;
 
-    const navigate = useNavigate();
-
-    const [ category, setCategory ] = useState([]);
-
-    const [ isLocked, setIsLocked ] = useState(false);
-
-    const categoryOnMouseEnter = (event) => {
-
-        if (!isLocked && (event.target.tagName == "LI")) {
-            event.target.classList.add('active');
-        }
-        /*
-        setIsLocked(true);
-            // 해당 요소 active 설정
-            timeoutId = setTimeout(() => {
-                setIsLocked(false);
-            }, 200);
-        */
-    };
-
-    const categoryOnMouseLeave = (event) => {
-        // 이렇게 하면 컴퓨터 상태에 따라서 부담이 갈수도 있겠지만 지금은 이게 훨씬 자연스러움
-        const items = document.querySelectorAll('.category_list_item');
-        items.forEach(item => {
-            item.classList.remove('active');
-        })
-    };
-
-    useEffect(() => {
-        const fetchCategory = async () => {
-            try {
-                const response = await axios.get('http://localhost:8080/category/major', {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('accessToken')}`
-                    },
-                    withCredentials: true
-                });
-
-                console.log(response);
-                setCategory(response.data); // 카테고리 저장
-            } catch (error) {
-                if (error.status == 403) {
-                    alert("카테고리를 가져오지 못함");
-                    navigate('/');
-                } else if (error.status == 401) {
-                    alert("카테고리를 가져오지 못함");
-                    navigate('')
-                }
-
-                console.log(error);
-                alert("유저를 검증할 수 없습니다.");
-                navigate('/'); // 만약 잘못된 토큰 제시로 보안상 문제가 있으면 홈 화면으로 이동
-            }
-        }
-
-        fetchCategory();
-    }, []);
-
     return (
         
-            <Container className="d-flex">
+            <Container className="wrapper">
 
-                <div className="catetory text-center" id="category">
-                    <ul className="category_list" role="menu">
-                        {category.map((item, index) => ( 
-
-                            <li id={`category${index}`} className="category_list_item" key={item.categoryId} 
-                            onMouseEnter={(e) => {categoryOnMouseEnter(e)}} 
-                            onMouseLeave={(e) => {categoryOnMouseLeave(e)}}>
-
-                                <a href="#">{item.categoryName}</a>
-
-                                {/* className = category_2depth */}
-                                <Category2Depth 
-                                    categoryId={item.categoryId}
-                                    children={item.children}
-                                />
-
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-
+                {/* 카테고리 네비게이션*/}
+                <CategoryNav />
+                
+                {/* 메인페이지 컨텐츠 */}
                 <div>
                     
                 </div>
