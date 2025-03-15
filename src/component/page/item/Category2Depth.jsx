9f31083e1dc7add2890b3fe94937c76a9b18f052
hyc4841 <dbscjf4841@naver.com>
2025-03-15 23:27:02 +0900

    카테고리 2중 네비게이션 hover 추가

--- src/component/page/item/Category2Depth.jsx
+++ src/component/page/item/Category2Depth.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../../css/categoryNav.css";
+import "../../../css/category2Depth.css";
 
 const Category2Depth = (props) => {
 
@@ -7,19 +7,40 @@ const Category2Depth = (props) => {
 
     const category2LayerId = `categoryHoverLayer${categoryId}`;
 
+
+    const category2DepthOnMouseEnter = (event) => {
+
+        if ((event.target.tagName == "LI")) {
+            event.target.classList.add('active');
+        }
+    };
+
+    const category2DepthOnMouseLeave = (event) => {
+
+        const items = document.querySelectorAll('.category_2depth_list_item');
+        items.forEach(item => {
+            item.classList.remove('active');
+        })
+
+    };
+
+
+
     return (
         <div id={category2LayerId} className="category_2depth" >
             
-            <ul>
+            <ul className="category_2depth_list">
 
                 {children && 
-                <>
-                    {children.map((item, index) => (
-                        <li key={item.categoryId}>
-                            <a href={`/products?category=${item.categoryId}`}>{item.categoryName}</a>
-                        </li>
-                    ))}
-                </>
+                    <>
+                        {children.map((item, index) => (
+                            <li className="category_2depth_list_item" key={item.categoryId}
+                                onMouseEnter={category2DepthOnMouseEnter}
+                                onMouseLeave={category2DepthOnMouseLeave}>
+                                <a href={`/products?category=${item.categoryId}`}>{item.categoryName}</a>
+                            </li>
+                        ))}
+                    </>
                 }
 
             </ul>
