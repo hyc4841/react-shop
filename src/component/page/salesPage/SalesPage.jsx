1a73643b56db72789ce2ef11c09013fc2e3e18af
hyc4841 <dbscjf4841@naver.com>
2025-04-28 23:33:38 +0900

    SalesPage. 판매 페이지 주문 상품 수량 제어 및 상품 삭제 로직 변경

--- src/component/page/salesPage/SalesPage.jsx
+++ src/component/page/salesPage/SalesPage.jsx
@@ -1,10 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { Form, FormSelect, Button } from "react-bootstrap";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import ItemOptionCard from "./ItemOptionCard";
 
 const SalesPage = () => {
+
+    const navigate = useNavigate();
 
     const location = useLocation();
     const query = new URLSearchParams(location.search);
@@ -12,14 +14,11 @@ const SalesPage = () => {
 
     const [ pageData, setPageData ] = useState('');
     const [ itemOptions, setItemOptions ] = useState('');
-    const [ selectedItems, setSelectedItems ] = useState([]);
+    const [ selectedItems, setSelectedItems ] = useState(new Map()); // selectedItems는 map 객체임
 
     const [ option1, setOption1 ] = useState('');
-    const [ option2, setOption2 ] = useState('')
-    const [ option3, setOption3 ] = useState('');
-
-    console.log("최상위 옵션 : ", itemOptions[0]);
-    console.log('선택한 아이템들 : ', selectedItems);
+    const [ option2, setOption2 ] = useState('');
+    const [ option3, setOption3 ] = useState(''); 
 
     useEffect(() => {
             const fetchPage = async () => {
@@ -33,7 +32,6 @@ const SalesPage = () => {
                     setPageData(response.data);
                     setItemOptions(response.data.page.itemOptionList);
 
-
                 } catch (error) {
                     console.error("페이지 응답을 가져오지 못함 : ", error);
                 }
@@ -46,6 +44,19 @@ const SalesPage = () => {
     const formSubmitHandler = (e) => {
         e.preventDefault();
 
+        console.log(selectedItems.size);
+
+        if (selectedItems.size > 0) {
+            const orderData = {
+                pageId: pageId,
+                orderItems: selectedItems, // map 객체. item - quantity
+                // 추후에 추가할 데이터
+            };
+    
+            navigate('/order/preview', { state: orderData });
+        } else {
+            alert("상품을 선택해주세요");
+        }
     };
 
     const selectHandler = (e, parent) => {
@@ -56,32 +67,47 @@ const SalesPage = () => {
         // 선택한 옵션 및에 하위 옵션이 있는지 확인해야한다.
         if (selectedOption.child != null) {
         }
-    };
-
-    const removeSelectedItem = (e, itemId) => {
-        console.log("삭제하려는 아이템 id : ", itemId);
-        const removeItem = selectedItems.find(item => item.itemId === itemId); // 이 로직에 문제가 있음
-
-        console.log("삭제하려는 아이템: ", removeItem);
-        setSelectedItems((prev) => prev.filter(item => item.itemId !== itemId));
 
     };
 
+    // 상품 삭제하기
+    const removeSelectedItem = (e, item) => {
+        console.log("삭제하려는 상품 : ", item);
+
+        const newSelectedItems  = new Map(selectedItems);
+        newSelectedItems.delete(item);
+
+        setSelectedItems(newSelectedItems);
+    };
+
+    // 상품 선택하기
     const selectItemHandler = (selectItem) => {
 
         console.log("기존에 선택된 상품들 : ", selectedItems);
-        const isItem = selectedItems.find(item => item.itemId === selectItem.itemId); // 이 로직에 문제가 있음.
-        console.log("기존 리스트에 존재해? : ", isItem);
-        
+        console.log("선택한 아이템이 기존에 선택한 아이템 리스트에 존재하나요? : ", selectedItems.has(selectItem));
 
-
-        if (isItem != null) { // 선택한 상품이 이미 선택한 상품 리스트에 있다면,
+        if (selectedItems.has(selectItem) == true) { // 선택한 상품이 이미 선택한 상품 리스트에 있다면,
             alert("이미 선택한 상품 입니다.");
         } else {
             console.log("상품 추가하기 : ", selectItem);
-            setSelectedItems(prev => [...prev, selectItem]); 
+            const newSelectedItems  = new Map(selectedItems);
+            newSelectedItems.set(selectItem, 1);
+
+            setSelectedItems(newSelectedItems);
         }
     };
+
+    // 상품 수량 변경 함수
+    const quantityChangeHandler = (e, item) => {
+
+
+        console.log("새로 입력된 값? : ", e.target.value);
+
+        const newSelectedItems  = new Map(selectedItems);
+        newSelectedItems.set(item,  Number(e.target.value));
+
+        setSelectedItems(newSelectedItems);
+    }
 
     
     return (
@@ -101,6 +127,7 @@ const SalesPage = () => {
                     {/* 이거 그냥 재귀 함수처럼 하면 그냥 되겠는데? */}
 
                     <div className="d-flex">
+                        {/* 상품 옵션 */}
                         <div>
                             {itemOptions[0]  &&
                                 <>
@@ -113,32 +140,34 @@ const SalesPage = () => {
                             }
                         </div>
 
+                        {/* 선택된 상품들 */}
                         <div>
                             {selectedItems && 
-
-                                selectedItems.map((item, index) => (
+                                [...selectedItems.entries()].map(([key, value], index) => (
                                     <div key={index}>
-                                        <p>{item.itemId}</p>
-                                        <p>{item.name}</p>
-                                        <p>{item.price}</p>
-                                        <p>{item.stockQuantity}</p>
-                                        <Button onClick={(e) => removeSelectedItem(e, item.itemId)}>삭제</Button>
+                                        <p>인덱스는 : {index}</p>
+                                        <p>{key.itemId}</p>
+                                        <p>상품명 : {key.name}</p>
+                                        <p>상품 가격 : {key.price}</p>
+                                        <p>보유 수량 : {key.stockQuantity}</p>
+
+                                        <input name="quantity" type="number" value={value} min={1} step={1} required onChange={(e) => quantityChangeHandler(e, key)} style={{display: "block", marginBottom: "10px"}}/>
+
+                                        <Button onClick={(e) => removeSelectedItem(e, key)}>삭제</Button>
                                     </div>
-
                                 ))
-                            
                             }
-                            
 
+                            <hr/>
+                            <Button onClick={formSubmitHandler}>주문하기</Button>
                         </div>
-
                     </div>
                     
-                    
+
+
                 </Form>
                 
             </div>
-
         </div>
 
 
