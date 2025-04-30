15affe0d0bca3f0ac5c1949a15aa368e98fb1877
hyc4841 <dbscjf4841@naver.com>
2025-04-30 15:17:42 +0900

    PrivateRoute. 이전 페이지에서 로그인 페이지로 이동할 때, 이전 페이지에서 가져온 데이터 보존 하는 로직

--- src/component/page/login/PrivateRoute.jsx
+++ src/component/page/login/PrivateRoute.jsx
@@ -6,6 +6,8 @@ const PrivateRoute = ({ children }) => {
     const location = useLocation();
     const isLoggedIn = localStorage.getItem('accessToken');
 
+    console.log("데이터 가져오기? : ", location.state);
+
     if (isLoggedIn == null) { // 로그인 안되어 있으면
 
         alert("로그인이 필요한 화면입니다. 로그인 페이지로 이동합니다.");
@@ -20,3 +22,6 @@ const PrivateRoute = ({ children }) => {
 export default PrivateRoute;
 
 // 이거 트러블 슈팅 거리다.
+// 보호된 라우트 컴포넌트 생성.
+// 이전 페이지로 이동시키기, 뒤로가기 안되도록 하기, 등등
+// 인증 요청, 인증 요청 이후 라우팅에 대한 고민
