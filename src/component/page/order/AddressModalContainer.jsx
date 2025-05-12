70283f0146fe258e199d4aeb55b2f89b3a39ea69
hyc4841 <dbscjf4841@naver.com>
2025-05-12 23:02:49 +0900

    AddressModalContainer 생성

---
+++ src/component/page/order/AddressModalContainer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import AddressPortal from "./AddressPortal";
+import { Button, Container } from "react-bootstrap";
+import "../../../css/addressModalContainer.css";
+
+const AddressModalContainer = ({ isOpen, onClose, children }) => {
+
+    if (!isOpen) {
+        return null;
+    }
+    
+    return (
+        <AddressPortal>
+            <div className="modal-overlay"> {/* onClick={onClose} 오버레이 클릭시 닫기 */}
+                <Container className="modal-content" onClick={(e) => e.stopPropagation()}> {/* 컨텐츠 영역 클릭 시 닫히지 않게 방지 */}
+
+                    <div className="d-flex justify-content-between">
+                        <h4 className="modal-title">주소 변경</h4>
+
+                        <button className="modal-close-button" onClick={onClose}>
+                            &times;
+                        </button>
+                    </div>
+
+                    <hr style={{marginTop: "0"}}/>
+                    
+                    {/* 모달에 표현되는 자식 */}
+                    {children}
+
+
+                </Container>
+            </div>
+        </AddressPortal>
+    );
+
+
+};
+
+
+
+export default AddressModalContainer;
