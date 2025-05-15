42220ae8b4cedd6d11e43d71097fca38b45265b3
hyc4841 <dbscjf4841@naver.com>
2025-05-15 23:35:19 +0900

    AddressAddModal. 주소 추가 모달 수정

--- src/component/page/member/AddressAddModal.jsx
+++ src/component/page/member/AddressAddModal.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import DaumPostModal from "../signup/DaumPostModal";
 import { addAddressRequest } from "../../../redux/reducer/userSlice";
 
@@ -12,17 +12,23 @@ const AddressAddModal = ({ isOpen, onCancel }) => {
     const [ zipcode, setZipcode ] = useState('');
     const [ street, setStreet ] = useState('');
     const [ detailedAddress, setDetailedAddress ] = useState('');
+    const [ addressName, setAddressName ] = useState('');
+
+    const { addressAddError }  = useSelector((state) => state.user);
+
     const [ daumModalOnOff, setDaumModalOnOff ] = useState(false);
 
 
+    // 주소 초기화 함수
+    const addressClean = () => {
+        setCity('');
+        setZipcode('');
+        setStreet('');
+        setDetailedAddress('');
+    }
 
     const addAddressRequestHandler = () => {
-        try {
-            dispatch(addAddressRequest(city, zipcode, street, detailedAddress));
-
-        } catch (error) {
-
-        }
+        dispatch(addAddressRequest({ zipcode, city, street, detailedAddress, addressName }));
     };
 
     const daumModalHandler = (e) => {
@@ -30,48 +36,94 @@ const AddressAddModal = ({ isOpen, onCancel }) => {
         setDaumModalOnOff(true);
     };
 
+    const modalCancleHandler = () => {
+        onCancel();
+        addressClean();
+    };
+
 
     if (!isOpen) {
         return null;
     }
 
+    console.log("에러 확인 : ", addressAddError);
+
     return (
         <div className="modal-overlay">
             <div className="modal-content">
-                <p>주소 추가하기</p>
+                
                 <div className="modal-actions">
-                    <Button onClick={onCancel}>취소</Button>
+                    <div className="d-flex justify-content-between">
+                        <p>주소 추가하기</p>
+                        <Button onClick={modalCancleHandler}>취소</Button>
+                    </div>
+                    
                     <Form>
+                        <div className="d-flex justify-content-between">
+
+                            <div>
+                                <Form.Group controlId="zipcode" style={{width: "48%"}}>
+                                    <Form.Label>우편번호</Form.Label>
+                                    <Form.Control type="text" value={zipcode} readOnly required />
+                                </Form.Group>
+
+                                {addressAddError &&
+                                    addressAddError.zipcode
+                                }
+                            </div>
+                            
+
+                            <Form.Group controlId="addressName" style={{width: "48%"}}>
+                                <Form.Label>주소 별명</Form.Label>
+                                <Form.Control type="text" value={addressName} onChange={(e) => setAddressName(e.target.value)} />
+                            </Form.Group>
+                        </div>
                         
-                        <Form.Group controlId="zipcode">
-                            <Form.Label>우편번호</Form.Label>
-                            <Form.Control type="text" value={zipcode} readOnly required />
-                        </Form.Group>
+
 
                         <Form.Group controlId="street">
                             <Form.Label>주소</Form.Label>
                             <Form.Control type="text" value={street} readOnly required />
                         </Form.Group>
 
-                        <Form.Group controlId="detailedAddress">
-                            <Form.Label>상세주소</Form.Label>
-                            <Form.Control type="text" value={detailedAddress} onChange={(e) => setDetailedAddress(e.target.value)} required />
-                        </Form.Group>
 
+                        <div>
+                            <Form.Group controlId="detailedAddress">
+                                <Form.Label>상세주소</Form.Label>
+                                <Form.Control type="text" value={detailedAddress} onChange={(e) => setDetailedAddress(e.target.value)} required />
+                            </Form.Group>
 
-                        <Button onClick={(e) => daumModalHandler(e)}>우편번호 찾기</Button>
+                            {addressAddError &&
+                                <div>
+                                    {addressAddError.detailedAddress?.map((item, index) => (
+                                        <p key={index} style={{color: "red"}}>{item.message}</p>
+                                    ))}
 
-                        <DaumPostModal 
+                                </div>
+                               
+                                
+                            }
+
+                        </div>
+                        
+
+                        <div style={{marginTop: "15px"}} className="d-flex justify-content-between">
+                            <Button  onClick={(e) => daumModalHandler(e)}>우편번호 찾기</Button>
+                            <Button  onClick={() => addAddressRequestHandler()}>추가하기</Button>
+                        </div>
+
+                    </Form>
+
+                    
+                    
+
+                    <DaumPostModal 
                             modalOnOff={daumModalOnOff}
                             setModalOnOff={setDaumModalOnOff}
                             setZipcode={setZipcode}
                             setCity={setCity}
                             setStreet={setStreet}
                         />
-
-                    </Form>
-
-                    <Button onClick={() => addAddressRequestHandler()}>추가하기</Button>
                 </div>
             </div>
         </div>
