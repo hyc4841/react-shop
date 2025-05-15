858c31154cccce2402d596e1b2c6a08be6172f53
hyc4841 <dbscjf4841@naver.com>
2025-05-16 00:47:32 +0900

    App. 필요 없는 코드 삭제

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import styled from 'styled-components';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import MainPage from './component/page/MainPage';
-import PostWritePage from './component/page/board/PostWritePage';
-import PostViewPage from './component/page/board/PostViewPage';
-
 import BoardPage from './component/page/board/BoardPage'
 import LoginPage from './component/page/login/LoginPage'
 import SignupPage from './component/page/signup/SignupPage';
-
 import MemberInfoPage from './component/page/member/MemberInfoPage';
-
 import Header from './component/ui/Header';
 import Footer from './component/ui/Footer';
 import ItemListPage from './component/page/item/ItemListPage';
 import SalesPage from './component/page/salesPage/SalesPage';
 import OrderPreviewPage from './component/page/order/OrderPreviewPage';
 import PrivateRoute from './component/page/login/PrivateRoute';
-
-const MainTitleText = styled.p`
-    font-size: 24px;
-    font-weight: bold;
-    text-align: center;
-`;
 
 function App(props) {
     return (
