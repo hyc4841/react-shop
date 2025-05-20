923ee97458a0a0ac3bf160e4ac578b254fb90a96
hyc4841 <dbscjf4841@naver.com>
2025-05-20 23:37:42 +0900

    AddressModalContent. 주소 직접 입력 시 발생하는 데이터 잔류 현상 수정.

--- src/component/page/order/AddressModalContent.jsx
+++ src/component/page/order/AddressModalContent.jsx
@@ -6,24 +6,35 @@ import DaumPostModal from "../signup/DaumPostModal";
 
 const AddressModalContent = (props) => {
 
-    const { selectedAddress, addressList, setSelectedAddress } = props;
+    const { selectedAddress, addressList, setSelectedAddress,
+        setZipcode, setCity, setStreet, setDetailedAddress,
+        zipcode, city, street, detailedAddress} = props;
+
+    const [ inputZipcode, setInputZipcode ] = useState(null);
+    const [ inputCity, setInputCity ] = useState(null);
+    const [ inputStreet, setInputStreet ] = useState(null);
+    const [ inputDetailedAddress, setInputDetailedAddress ] = useState(null);
+
+    const [ newAddressFieldError, setNewAddressFieldError ] = useState(null);
 
     const [ isPopupOpen, setIsPopupOpen ] = useState(false);
 
     const [ daumModalOnOff, setDaumModalOnOff] = useState(false);
 
+    // newAddressInputOnOff. true : 직접 입력하기 버튼 활성화, false : 주소 입력창 활성화
     const [ newAddressInputOnOff, setNewAddressInputOnOff ] = useState(true);
 
-    const [ zipcode, setZipcode ] = useState('');
-    const [ city, setCity ] = useState('');
-    const [ street, setStreet ] = useState('');
-    const [ detailedAddress, setDetailedAddress ] = useState('');
-
+    const inputAddressCleanUp = () => {
+        setInputZipcode(null);
+        setInputCity(null);
+        setInputStreet(null);
+        setInputDetailedAddress(null);
+    };
     const addressCleanUp = () => {
-        setZipcode('');
-        setCity('');
-        setStreet('');
-        setDetailedAddress('');
+        setZipcode(null);
+        setCity(null);
+        setStreet(null);
+        setDetailedAddress(null);
     };
 
     const handleOpenPopup = () => {
@@ -32,6 +43,9 @@ const AddressModalContent = (props) => {
 
     const handleClosePopup = () => {
         setIsPopupOpen(false);
+        setNewAddressInputOnOff(true);
+        inputAddressCleanUp();
+        setNewAddressFieldError(null);
     };
 
     // 주소 선택 버튼
@@ -39,6 +53,8 @@ const AddressModalContent = (props) => {
         setSelectedAddress(item);
         // 주소를 선택하면 직접 입력 부분은 null로 초기화 해주기
         addressCleanUp();
+        inputAddressCleanUp();
+        setNewAddressFieldError(null);
         handleClosePopup();
     };
 
@@ -51,7 +67,8 @@ const AddressModalContent = (props) => {
             // 버튼 활성화/직접 입력 비활성화
             setNewAddressInputOnOff(true);
             // 입력했던거 전부 삭제하기
-            addressCleanUp();
+            inputAddressCleanUp();
+            setNewAddressFieldError(null);
         }
     };
 
@@ -64,8 +81,24 @@ const AddressModalContent = (props) => {
     // 주소 직접 입력. 확인 버튼
     const addressConfirmHandler = (e) => {
         e.preventDefault();
-        setNewAddressInputOnOff(true);
-        setSelectedAddress(''); // 주소 직접 입력 모드는 선택한 주소를 제거한다.
+
+        // 주소 필드중 하나라도 입력을 안하면 오류 필드를 등록한다.
+        if (inputCity != null && inputZipcode != null && inputStreet != null && inputDetailedAddress != null) {
+
+            setNewAddressInputOnOff(true);
+            setSelectedAddress(null); // 주소 직접 입력 모드는 선택한 주소를 제거한다.
+
+            setCity(inputCity);
+            setZipcode(inputZipcode);
+            setStreet(inputStreet);
+            setDetailedAddress(inputDetailedAddress);
+
+            setNewAddressFieldError('');
+            handleClosePopup();
+        } else {
+            setNewAddressFieldError("상세 주소까지 모두 입력해주세요");
+        }
+
     };
 
     return (
@@ -77,14 +110,21 @@ const AddressModalContent = (props) => {
                 </div>
             }
 
-            <Button onClick={handleOpenPopup}>주소 변경</Button>
+            {city &&
+                <>
+                    <p>배송지 직접 입력</p>
+                    <p>주소 : {city} {street} {detailedAddress}</p>
+                </>
+            }
+
+            <Button onClick={handleOpenPopup}>주소 선택</Button>
 
             <AddressModalContainer isOpen={isPopupOpen} onClose={handleClosePopup}>
 
                 {addressList &&
                     addressList?.map((item, index) => (
-                        <div className="d-flex justify-content-between">
-                            <div className="d-flex align-items-center" key={index}>
+                        <div className="d-flex justify-content-between" key={index}>
+                            <div className="d-flex align-items-center" >
                                 <p style={{marginBottom: "0"}}>배송지 : {item.city} {item.street} {item.detailedAddress}</p>
                             </div>
                             <Button onClick={(e) => buttonClickHandler(e, item)}>선택</Button> 
@@ -103,14 +143,18 @@ const AddressModalContent = (props) => {
                                 <Button onClick={() => newAddressInputOnOffHandler()}>취소</Button>
                             </div>
 
+                            {newAddressFieldError && 
+                                <p style={{color: "red"}}>{newAddressFieldError}</p>
+                            }
+
                             <div className="input-area">
                                 <form>
                                     <label htmlFor="zipcode" >우편번호</label>
-                                    <input className="form-control" id="zipcode" value={zipcode} readOnly required/>
+                                    <input className="form-control" id="zipcode" value={inputZipcode} readOnly required/>
                                     <label htmlFor="street" >주소</label>
-                                    <input className="form-control" id="street" value={street} readOnly required/>
+                                    <input className="form-control" id="street" value={inputStreet} readOnly required/>
                                     <label htmlFor="detailedAddress" >상세주소</label>
-                                    <input className="form-control" id="detailedAddress" value={detailedAddress} onChange={(e) => setDetailedAddress(e.target.value)} required/>
+                                    <input className="form-control" id="detailedAddress" value={inputDetailedAddress} onChange={(e) => setInputDetailedAddress(e.target.value)} required/>
 
                                     <div className="d-flex justify-content-between">
                                         <Button style={{marginTop: "15px"}} onClick={(e) => daumModalHandler(e)}>우편번호 찾기</Button>
@@ -120,15 +164,13 @@ const AddressModalContent = (props) => {
                                 </form>
                                 
                             </div>
-                            
-                            
 
                             <DaumPostModal 
                                 modalOnOff={daumModalOnOff}
                                 setModalOnOff={setDaumModalOnOff}
-                                setZipcode={setZipcode}
-                                setCity={setCity}
-                                setStreet={setStreet}
+                                setZipcode={setInputZipcode}
+                                setCity={setInputCity}
+                                setStreet={setInputStreet}
                             />
 
                         </div>
