5cd463c15f5e0c5e293e0da688c34a1495c9dec5
hyc4841 <dbscjf4841@naver.com>
2025-05-22 23:03:23 +0900

    LoginChangeButton. 로그인 아이디 변경 요청 로직 수정

--- src/component/page/member/LoginIdChangeButton.jsx
+++ src/component/page/member/LoginIdChangeButton.jsx
@@ -3,47 +3,28 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getMemberData } from "../../../redux/reducer/userSlice";
+import { loginIdUpdate } from "../../../redux/reducer/userSlice";
 
 const LoginChangeButton = (props) => {
 
-    const { newLoginId, onChangeError } = props;
+    const { newLoginId } = props;
     const navigate = useNavigate();
     const dispatch = useDispatch();
 
     const submitLoginIdUpdate = async () => {
-        console.log("newLoginId : " + newLoginId);
+    
+        const resultAction = await dispatch(loginIdUpdate({ newLoginId: newLoginId }));
 
-        try {
-            dispatch(getMemberData());
-            /*
-
-            const response = await axios.post('http://localhost:8080/member/id', 
-                { newLoginId }, 
-                {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-                        'Content-Type': 'application/json',
-                    },
-                    withCredentials: true,
-                }
-            );
-            console.log(response);
-            alert("아이디가 성공적으로 변경 되었습니다.");
-            */
+        if (loginIdUpdate.fulfilled.match(resultAction)) {
+            alert("아이디가 성공적으로 변경되었습니다.");
             navigate(0);
-        } catch (error) {
-            onChangeError(error.response.data);
-            console.log(error.response.data);
         }
     };
-
 
     return (
         <Button type="button" onClick={submitLoginIdUpdate}>
             변경
         </Button>
-
     );
 };
 
