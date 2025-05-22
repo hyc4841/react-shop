b9112eae3fff5b4382c45cff1687d2313c3cfb71
hyc4841 <dbscjf4841@naver.com>
2025-05-22 23:04:27 +0900

    UpdateEmailCodeSubmitbutton. 이메일 변경 인증 코드 확인 로직 수정

--- src/component/page/member/UpdateEmailCodeSubmitButton.jsx
+++ src/component/page/member/UpdateEmailCodeSubmitButton.jsx
@@ -3,15 +3,13 @@ import React from "react";
 import { Button } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { getMemberData } from "../../../redux/reducer/userSlice";
+import { emailUpdate } from "../../../redux/reducer/userSlice";
 
 const UpdateEmailCodeSubmitbutton = (props) => {
-
     const { email, code, setError} = props;
 
     const navigate = useNavigate();
     const dispatch = useDispatch();
-
 
     const submitEmailCode = async () => {
         
@@ -27,8 +25,10 @@ const UpdateEmailCodeSubmitbutton = (props) => {
                 });
                 console.log("이메일 인증 요청 응답 : ", response);
 
-                updateEmail(); // 이메일 인증에 성고하면 바로 이메일 수정으로 요청보냄
-                
+                dispatch(emailUpdate({ newEmail: email }));
+                alert("이메일 수정에 성공했습니다");
+                navigate(0);
+                // updateEmail(); // 이메일 인증에 성고하면 바로 이메일 수정으로 요청보냄
 
         } catch (error) {
             console.error(error);
@@ -37,11 +37,10 @@ const UpdateEmailCodeSubmitbutton = (props) => {
     };
 
 
+    /*
     const updateEmail = async () => {
 
         try {
-            dispatch(getMemberData());
-            /*
             const response = await axios.post('http://localhost:8080/member/email',
                 { newEmail: email },
                 {
@@ -50,18 +49,18 @@ const UpdateEmailCodeSubmitbutton = (props) => {
                         "Content-Type": "application/json",
                     },
                     withCredentials: true
-                });
+                }
+            );
 
                 console.log("이메일 업데이트 성공 : ", response);
                 setMemberData(response.data);
                 alert("이메일 수정에 성공했습니다");
                 navigate(0); // 페이지 새로고침
-                */
         } catch (error) {
             console.error(error);
         }
-
     };
+    */
 
 
     return (
