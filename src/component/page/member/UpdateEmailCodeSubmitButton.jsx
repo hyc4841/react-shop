41460c3c84917cc900dd0e0d0aa78fee86958771
hyc4841 <dbscjf4841@naver.com>
2025-05-23 00:13:22 +0900

    UpdateEmailCodeSubmitbutton. 수정 작업 진행중

--- src/component/page/member/UpdateEmailCodeSubmitButton.jsx
+++ src/component/page/member/UpdateEmailCodeSubmitButton.jsx
@@ -23,45 +23,20 @@ const UpdateEmailCodeSubmitbutton = (props) => {
                     },
                     withCredentials: true
                 });
+
                 console.log("이메일 인증 요청 응답 : ", response);
 
-                dispatch(emailUpdate({ newEmail: email }));
-                alert("이메일 수정에 성공했습니다");
-                navigate(0);
-                // updateEmail(); // 이메일 인증에 성고하면 바로 이메일 수정으로 요청보냄
+                if (response.status == 200) {
+                    dispatch(emailUpdate({ newEmail: email }));
+                    alert("이메일 수정에 성공했습니다");
+                    // navigate(0);
+                }
 
         } catch (error) {
             console.error(error);
             setError(error.response.data);
         }
     };
-
-
-    /*
-    const updateEmail = async () => {
-
-        try {
-            const response = await axios.post('http://localhost:8080/member/email',
-                { newEmail: email },
-                {
-                    headers: {
-                        Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
-                        "Content-Type": "application/json",
-                    },
-                    withCredentials: true
-                }
-            );
-
-                console.log("이메일 업데이트 성공 : ", response);
-                setMemberData(response.data);
-                alert("이메일 수정에 성공했습니다");
-                navigate(0); // 페이지 새로고침
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    */
-
 
     return (
         <Button onClick={submitEmailCode} style={{whiteSpace: "nowrap", marginLeft: "15px"}}>
